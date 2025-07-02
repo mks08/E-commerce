da97bebe6e66efe8ab99ea0113f764e6459e2edb
Mayank Kumar Singh <92930112+mks08@users.noreply.github.com>
2025-07-02 12:01:08 +0530

    login.component.ts

--- login.component.ts
+++ login.component.ts
@@ -2,28 +2,33 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { finalize } from 'rxjs/operators';
-import { AuthService } from '../../../../core/auth.service';
-import { NotificationService } from '../../../../shared/notification.service';
+import { AuthService } from '../../../core/services/auth.service';
+import { NotificationService } from '../../../core/services/notification.service';
 
 @Component({
   selector: 'app-login',
-  standalone : false,
+  standalone: false, 
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+
   loginForm: FormGroup;
+
+  //disable button during API call
   isLoading = false;
 
   constructor(
-    private fb: FormBuilder,
-    private authService: AuthService,
-    private router: Router,
-    private notificationService: NotificationService
+    private fb: FormBuilder,                    
+    private authService: AuthService,           
+    private router: Router,                     
+    private notificationService: NotificationService 
   ) {
+    
+    // form controls and validators
     this.loginForm = this.fb.group({
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(6)]]
+      email: ['', [Validators.required, Validators.email]], 
+      password: ['', [Validators.required, Validators.minLength(6)]] 
     });
   }
 
@@ -32,13 +37,16 @@ export class LoginComponent {
       return;
     }
     this.isLoading = true;
+    
     const { email, password } = this.loginForm.value;
-
+    // Sends the data to login in AuthService , uses the user object and performs next operation.
     this.authService.login(email, password).pipe(
-      finalize(() => this.isLoading = false)
-    ).subscribe({
+      finalize(() => this.isLoading = false) 
+    ).subscribe({ 
       next: (user) => {
         this.notificationService.showSuccess('Login successful!');
+
+        // Redirect user based on role
         const redirectUrl = user.role === 'admin' ? '/dashboard/admin' : '/products';
         this.router.navigate([redirectUrl]);
       },
@@ -48,6 +56,7 @@ export class LoginComponent {
     });
   }
 
+ // Signup link
   goToSignup() {
     this.router.navigate(['/auth/register']);
   }
