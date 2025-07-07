853f804cd432b27b48bbbb7e3ef18dfd7adbdf0d
Mayank Kumar Singh <92930112+mks08@users.noreply.github.com>
2025-07-07 11:43:04 +0530

    auth-routing.module.ts

--- auth-routing.module.ts
+++ auth-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './registration/registration.component';
 
+// Routing Path
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
